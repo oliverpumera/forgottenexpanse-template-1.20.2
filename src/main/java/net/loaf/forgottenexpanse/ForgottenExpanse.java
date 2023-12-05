64ea3ac0b474a71a61ce6e638b4c69f4e6ed8560
Oliver Pumera <oliverpumera@Someones-Computer.local>
2023-12-05 14:28:22 +0100

    Initial commit

---
+++ src/main/java/net/loaf/forgottenexpanse/ForgottenExpanse.java
@@ -0,0 +1,16 @@
+package net.loaf.forgottenexpanse;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ForgottenExpanse implements ModInitializer {
+	public static final String MOD_ID = "forgottenexpanse";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/loaf/forgottenexpanse/ForgottenExpanseClient.java
@@ -0,0 +1,10 @@
+package net.loaf.forgottenexpanse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForgottenExpanseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
